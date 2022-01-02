4a8ceb1ae1c442b0c044fbd28a8fc616cb55fcfd
simonittensammer <simon.ittensammer@gmx.at>
2022-01-02 15:55:27 +0100

    add entity Pinboard

---
+++ digiboard-backend/src/main/java/at/htl/entity/Pinboard.java
@@ -0,0 +1,52 @@
+package at.htl.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "digi_pbd")
+public class Pinboard extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+
+    @OneToMany
+    List<Note> notes;
+
+    public Pinboard() {
+    }
+
+    public Pinboard(String name) {
+        this.name = name;
+        this.notes = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void addNote(Note note) {
+        this.notes.add(note);
+    }
+}
